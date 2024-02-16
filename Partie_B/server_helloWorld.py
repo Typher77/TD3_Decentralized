7af9ba06be4eed19dd61eb9a2ab922b1e39dd814
unknown <penelope.peronnet@edu.devinci.fr>
2024-02-16 09:32:12 +0100

    partie b get product et get product id

--- Partie_B/server_helloWorld.py
+++ Partie_B/server_helloWorld.py
@@ -3,10 +3,12 @@ from flask import Flask
 # Create an instance of Flask app
 app = Flask(__name__)
 
+
 # Define a route that responds with "Hello, World!"
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
 
 # Run the server
 if __name__ == '__main__':
