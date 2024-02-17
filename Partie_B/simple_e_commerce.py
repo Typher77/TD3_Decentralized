3812612e97b1387e5c3d731b14eba936d95b56b8
unknown <penelope.peronnet@edu.devinci.fr>
2024-02-17 15:05:43 +0100

    adding the put and delete requests for products

--- Partie_B/simple_e_commerce.py
+++ Partie_B/simple_e_commerce.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify,request
-from flask_cors import CORS
 import sqlite3
 
 app = Flask(__name__)
@@ -15,9 +14,7 @@ def get_db_connection():
 @app.route('/')
 def hello_world():
     return '''<h1>Welcome to our simple e-commerce app</h1>
-            <h1>products: get the list of products</h1>
-            <h1>products/id : get the product identified by its id</h1>
-            <h1>add product: POST product</h1>
+            <h1>To test the different routes, you can use Postmann</h1>
 
 '''
 
@@ -27,8 +24,7 @@ def get_products():
     conn = get_db_connection()
     cursor = conn.cursor()
     
-   
-    cursor.execute("SELECT * FROM products ORDER BY inStock DESC")
+    cursor.execute("SELECT * FROM products ORDER BY product_id")
    
     products = cursor.fetchall()
     conn.close()
@@ -37,7 +33,7 @@ def get_products():
 
 
 # get products/id 
-@app.route('/products/<int:id>', methods=['GET'])
+@app.route('/products/:<int:id>', methods=['GET'])
 def get_product(id):
     conn = get_db_connection()
     cursor = conn.cursor()
@@ -51,44 +47,76 @@ def get_product(id):
         return jsonify({"error": "Product not found"}), 404
 
 #post products
-@app.route('/add_product', methods=['GET', 'POST'])
+@app.route('/products', methods=['POST'])
 def add_product():
-    # handle the POST request
-    if request.method == 'POST':
-        name = request.form.get('name')
-        description = request.form.get('description')
-        price = request.form.get('price')
-        category = request.form.get('category')
-        inStock = request.form.get('inStock')
-        
-        #creating the new object
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT count(*) FROM products")
-        product_id = cursor.fetchone()[0]
-
-        cursor.execute('''
-    INSERT INTO Products(product_id, product_name, product_description, product_price, category, inStock ) VALUES 
-(?,?,?,?,?,?)''', (product_id + 1, name, description, price, category, inStock))
-        
-        conn.commit()
-        cursor.execute("SELECT * FROM products WHERE product_id = ?", (product_id+1,))
+    data=request.get_json()
    
-        products = cursor.fetchall()
-        conn.close()
+    name = data['product_name']
+    description = data['product_description']
+    price = data['product_price']
+    category = data['category']
+    inStock = data['inStock']
 
-        return jsonify([dict(row) for row in products])
-       
-       # form to get the new object information
-    return '''
-              <form method="POST">
-                  <div><label>product name: <input type="text" name="name"></label></div>
-                  <div><label>product description: <input type="text" name="description"></label></div>
-                  <div><label>product price : <input type="number" name="price"></label></div>
-                  <div><label>product category: <input type="text" name="category"></label></div>
-                  <div><label>product stock state : <input type="number" name="inStock"></label></div>
-                  <input type="submit" value="Submit">
-              </form>'''
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT count(*) FROM products")
+    product_id = cursor.fetchone()[0]
+
+    cursor.execute('''
+    INSERT INTO Products(product_id, product_name, product_description, product_price, category, inStock ) VALUES 
+    (?,?,?,?,?,?)''', (product_id + 1, name, description, price, category, inStock))
+        
+    conn.commit()
+    cursor.execute("SELECT * FROM products WHERE product_id = ?", (product_id+1,))
+   
+    products = cursor.fetchall()
+    conn.close()
+
+    return jsonify([dict(row) for row in products])     
+        
+              
+# put products
+@app.route('/products/:<int:id>', methods=['PUT'])
+def put_product(id):
+   data=request.get_json()
+   
+   name = data['product_name']
+   description = data['product_description']
+   price = data['product_price']
+   category = data['category']
+   inStock = data['inStock']
+   
+   conn = get_db_connection()
+   cursor = conn.cursor()
+   
+   cursor.execute('''
+                  UPDATE products SET product_name=?, product_description= ?, product_price= ?, category = ?, inStock= ? WHERE product_id = ?
+                  ''', (name, description, price, category, inStock,id))
+   
+   conn.commit()
+   cursor.execute("SELECT * FROM products WHERE product_id = ?", (id,))
+   
+   products = cursor.fetchall()
+   conn.close()
+
+   return jsonify([dict(row) for row in products])
+
+# delete products
+@app.route('/products/:<int:id>', methods=['DELETE'])
+def delete_product(id):
+    
+    conn = get_db_connection()
+    cursor = conn.cursor()
+   
+    cursor.execute('''
+                   DELETE FROM products WHERE product_id = ?
+                  ''', (id,))
+   
+    conn.commit()
+    conn.close()
+    return ' The product was deleted '
+
+    
 
 if __name__ == '__main__':
     app.run(host='localhost', port=3001)
